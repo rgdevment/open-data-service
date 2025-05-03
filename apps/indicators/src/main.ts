3424f0a3a8be474b138a7026516827e6fe12aa2e
rgdevment <mhidalgog@falabella.com>
2025-05-03 01:21:01 -0400

    feat: add database module

---
+++ apps/indicators/src/main.ts
@@ -0,0 +1,10 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap(): Promise<void> {
+  process.env.DB_NAME_ENV = 'COUNTRIES_DB';
+
+  const app = await NestFactory.create(AppModule);
+  await app.listen(process.env.PORT ?? 3000);
+}
+void bootstrap();
---
+++ libs/database/src/database.module.ts
@@ -0,0 +1,30 @@
+// libs/database/src/database.module.ts
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    ConfigModule,
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        const dbKey = config.get<string>('DB_NAME_ENV');
+        const dbName = config.get<string>(dbKey ?? 'MYSQL_DATABASE');
+
+        return {
+          type: 'mysql',
+          host: config.get<string>('MYSQL_HOST'),
+          port: parseInt(config.get<string>('MYSQL_PORT') ?? '3306'),
+          username: config.get<string>('MYSQL_USER'),
+          password: config.get<string>('MYSQL_ROOT_PASSWORD'),
+          database: dbName,
+          autoLoadEntities: true,
+          relationLoadStrategy: 'query',
+          synchronize: false,
+        };
+      },
+    }),
+  ],
+})
+export class DatabaseModule {}
